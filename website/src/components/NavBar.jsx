2f4a409716ff74968f4a38161bf8960a877c3be5
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-05 03:21:16 -0400

    fix

--- website/src/components/NavBar.jsx
+++ website/src/components/NavBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+         <div>
+            <h1 className="text-5xl font-signature ml-2">Yash</h1>
+         </div>
+    </div>
   )
 }
 
-export default NavBar
+export default NavBar;
